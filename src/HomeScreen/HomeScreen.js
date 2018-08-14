dd34cc6b132d8936bb5c49dbf7d16af1da813120
danlugo92 <danlugo92@gmail.com>
2018-08-14 14:00:31 -0400

    closes #29 new homescreen delete tab mockups

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -1,16 +1,35 @@
 import React from 'react'
 
-import { Image, Text } from 'react-native'
 import { Body, Button, Container, Header, Left, Right, Tab, TabHeading,
          Tabs } from 'native-base'
-
-import Tab1 from '../TabOne/TabOne'
-import Tab2 from '../TabTwo/TabTwo'
-import Tab3 from '../TabThree/TabThree'
+import { Image, Text, View } from 'react-native'
+/**
+ * @typedef {import('react-navigation').NavigationScreenProp<NavigationState>} NavigationScreenProp
+ */
 
 import styles from './style'
 
-const HomeScreenHeader = ({ navigation }) => (
+/**
+ * @typedef {import('../definitions').NavigationState} NavigationState
+ * @typedef {import('../definitions').NewsItem} NewsItem
+ */
+import Loader from '../utils/Loader'
+
+import CardsTabController from '../CardsTab/CardsTabController'
+import { fetchLatestNews, fetchRegionNews, fetchMostSeenNews } from '../CardsTab/CardsTabActions'
+
+
+/**
+ * @typedef HomeScreenProps
+ * @prop {NavigationScreenProp} navigation Navigation screen prop
+ */
+
+
+/**
+ *
+ * @param {NavigationScreenProp} navigation Navigation screen prop
+ */
+const HomeScreenHeader = navigation => (
   <Header>
     <Left>
       <Button
@@ -34,15 +53,21 @@ const HomeScreenHeader = ({ navigation }) => (
   </Header>
 )
 
-export default ({ navigation }) => (
+/**
+ * Renders the homescreen with the three main tabs (lo ultimo, regiones and
+ * lo mas visto)
+ * @param {HomeScreenProps} props
+ */
+const HomeScreen = ({ navigation }) => (
   <Container>
-    {HomeScreenHeader({ navigation })}
+    {HomeScreenHeader(navigation)}
     <Tabs
       edgeHitWidth={0}
       initialPage={0}
       tabBarUnderlineStyle={styles.underLineColor}
       tabContainerStyle={styles.tabContainerStyle}
     >
+      { /* LATEST NEWS TAB */ }
       <Tab
         heading={(
           <TabHeading style={styles.tabContainer}>
@@ -52,8 +77,49 @@ export default ({ navigation }) => (
           </TabHeading>
         )}
       >
-        <Tab1 navigation={navigation} />
+        <Loader
+          fetcherFunction={() => fetchLatestNews(1)}
+          loadingElement={(
+            <View
+              style={styles.deadCenter}
+            >
+              <Text>
+                CARGANDO
+              </Text>
+            </View>
+          )}
+          signalErrorFn={() => {}}
+          timeout={10000}
+        >
+          {
+            (newsItems, err) => {
+              if (err) {
+                return (
+                  <View
+                    style={styles.deadCenter}
+                  >
+                    <Text>
+                      ERROR DE CONEXION O TIMEOUT
+                    </Text>
+                  </View>
+                )
+              }
+              return (
+                <CardsTabController
+                  fetcher={(() => {
+                    let lastPageFetched = 1
+                    return () => fetchLatestNews(++lastPageFetched)
+                  })()}
+                  initialNewsItems={/** @type {NewsItem[]} */ (newsItems)}
+                  navigation={navigation}
+                />
+              )
+            }
+          }
+        </Loader>
       </Tab>
+
+      { /* MOST SEEN NEWS TAB */ }
       <Tab
         heading={(
           <TabHeading style={styles.tabContainer}>
@@ -63,8 +129,49 @@ export default ({ navigation }) => (
           </TabHeading>
         )}
       >
-        <Tab2 navigation={navigation} />
+        <Loader
+          fetcherFunction={() => fetchMostSeenNews(1)}
+          loadingElement={(
+            <View
+              style={styles.deadCenter}
+            >
+              <Text>
+                CARGANDO
+              </Text>
+            </View>
+          )}
+          signalErrorFn={() => {}}
+          timeout={10000}
+        >
+          {
+            (newsItems, err) => {
+              if (err) {
+                return (
+                  <View
+                    style={styles.deadCenter}
+                  >
+                    <Text>
+                      ERROR DE CONEXION O TIMEOUT
+                    </Text>
+                  </View>
+                )
+              }
+              return (
+                <CardsTabController
+                  fetcher={(() => {
+                    let lastPageFetched = 1
+                    return () => fetchMostSeenNews(++lastPageFetched)
+                  })()}
+                  initialNewsItems={/** @type {NewsItem[]} */ (newsItems)}
+                  navigation={navigation}
+                />
+              )
+            }
+          }
+        </Loader>
       </Tab>
+
+      { /* REGIONES NEWS TAB */ }
       <Tab
         heading={(
           <TabHeading style={styles.tabContainer}>
@@ -74,8 +181,49 @@ export default ({ navigation }) => (
           </TabHeading>
         )}
       >
-        <Tab3 navigation={navigation} />
+        <Loader
+          fetcherFunction={() => fetchRegionNews(1)}
+          loadingElement={(
+            <View
+              style={styles.deadCenter}
+            >
+              <Text>
+                CARGANDO
+              </Text>
+            </View>
+          )}
+          signalErrorFn={() => {}}
+          timeout={10000}
+        >
+          {
+            (newsItems, err) => {
+              if (err) {
+                return (
+                  <View
+                    style={styles.deadCenter}
+                  >
+                    <Text>
+                      ERROR DE CONEXION O TIMEOUT
+                    </Text>
+                  </View>
+                )
+              }
+              return (
+                <CardsTabController
+                  fetcher={(() => {
+                    let lastPageFetched = 1
+                    return () => fetchRegionNews(++lastPageFetched)
+                  })()}
+                  initialNewsItems={/** @type {NewsItem[]} */ (newsItems)}
+                  navigation={navigation}
+                />
+              )
+            }
+          }
+        </Loader>
       </Tab>
     </Tabs>
   </Container>
 )
+
+export default HomeScreen
--- src/HomeScreen/index.js
+++ src/HomeScreen/index.js
@@ -14,5 +14,5 @@ export default DrawerNavigator(
   },
   {
     contentComponent: props => <SideBar {...props} />,
-  },
+  }
 )
