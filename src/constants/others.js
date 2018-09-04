58e2097fa307fedd29b88629dadce5ae50c30cb4
danlugo92 <danlugo92@gmail.com>
2018-09-04 16:31:10 -0400

    add constants/others

---
+++ src/constants/others.js
@@ -0,0 +1,6 @@
+/**
+ * Key for AsyncStorage where the last date in which offline content was
+ * downloaded will be saved.
+ * @type {string}
+ */
+export const LAST_DATE_UPDATED_STOR_KEY = 'LAST_DATE_UPDATED_STOR_KEY'
