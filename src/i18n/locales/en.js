e2777042a6b0d66b3c1b40984c8086acc9ae2595
Jose Villalobos <joseavv4@gmail.com>
2018-09-11 00:54:15 -0400

    add i18next

---
+++ src/i18n/locales/en.js
@@ -0,0 +1,18 @@
+export default {
+  LOGIN: {
+    signIn: 'Sign In',
+    signUp: 'Sign Up',
+    email: 'Email',
+    password: 'Password',
+    emptyEmail: 'Empty email',
+    emptyPassword: 'Empty password',
+    forgotPassword: 'Forgot password?',
+  },
+  REGISTER: {
+    signUp: 'Sign Up',
+    email: 'Email',
+    password: 'Password',
+    goBack: 'Go back',
+    youHaveRegistered: 'You have signed up!',
+  },
+}
