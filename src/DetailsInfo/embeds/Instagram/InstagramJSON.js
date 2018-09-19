7329361e1aad1ffbabed719fa0ae67df3c805055
danlugo92 <danlugo92@gmail.com>
2018-09-19 12:36:52 -0400

    Instagram embed implementation pending style refinements

---
+++ src/DetailsInfo/embeds/Instagram/InstagramJSON.js
@@ -0,0 +1,41 @@
+/**
+ * @typedef {object} InstagramJSON
+ * @prop {number} author_id
+ * @prop {string} author_name
+ * @prop {string} author_url
+ * @prop {number|null} height
+ * @prop {string} html
+ * @prop {string} media_id
+ * @prop {string} provider_name
+ * @prop {string} provider_url
+ * @prop {string} title
+ * @prop {string} type
+ * @prop {string} thumbnail_url
+ * @prop {number} thumbnail_width
+ * @prop {number} thumbnail_height
+ * @prop {string} version
+ * @prop {number} width
+ */
+/*
+  https://www.instagram.com/developer/embedding/#oembed
+  {
+    "author_id": 9538472,
+    "author_name": "diegoquinteiro",
+    "author_url": "http://instagram.com/diegoquinteiro",
+    "height": null,
+    "html":"...",
+    "media_id": "558717847597368461_9538472",
+    "provider_name": "Instagram",
+    "provider_url": "http://instagram.com/",
+    "title": "Wii Gato (Lipe Sleep)",
+    "type": "rich",
+    "thumbnail_url": "http://distilleryimage5.ak.instagram.com/5dceebb02c5811e3b57222000a9e07e9_8.jpg",
+    "thumbnail_width": 640,
+    "thumbnail_height": 640,
+    "version": "1.0",
+    "width": 658
+  }
+*/
+
+// stop typescript from bugging
+export default {}
