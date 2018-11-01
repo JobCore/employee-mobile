329201344722b04c0f432bef30a13228a27cd07f
Jose Agustin Villalobos <joseavv4@gmail.com>
2018-11-01 11:05:49 -0400

    Update MainApplication.java

--- android/app/src/main/java/co/jobcore/talent/MainApplication.java
+++ android/app/src/main/java/co/jobcore/talent/MainApplication.java
@@ -28,10 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFirebasePackage(),
-            new RNFirebasePackage(),
             new RNFirebaseMessagingPackage(),
             new RNFirebaseNotificationsPackage(),
-            new MapsPackage(),
             new MapsPackage()
       );
     }
