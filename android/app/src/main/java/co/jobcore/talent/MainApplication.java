6882f33ac47279af377c298858dd7e3a70ffc4ef
Jose Agustin Villalobos <joseavv4@gmail.com>
2018-11-02 15:57:04 -0400

    Update MainApplication.java

--- android/app/src/main/java/co/jobcore/talent/MainApplication.java
+++ android/app/src/main/java/co/jobcore/talent/MainApplication.java
@@ -3,8 +3,6 @@ package co.jobcore.talent;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.maps.MapsPackage;
-import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
@@ -29,8 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage(),
-            new RNFirebasePackage(),
             new RNFirebasePackage(),
             new RNFirebaseMessagingPackage(),
             new RNFirebaseNotificationsPackage(),
