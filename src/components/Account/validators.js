526ba4fd99560f78053b6fc1a7e5f3bc26e84676
Jose Villalobos <joseavv4@gmail.com>
2018-12-18 14:00:08 -0400

    format code

--- src/components/Account/validators.js
+++ src/components/Account/validators.js
@@ -2,50 +2,48 @@ import * as utils from '../../utils';
 import { i18next } from '../../i18n';
 
 const loginValidator = (email, password) => {
-    if (!utils.isValidString(email)) {
-        throw new Error(i18next.t('LOGIN.emptyEmail'));
-    }
+  if (!utils.isValidString(email)) {
+    throw new Error(i18next.t('LOGIN.emptyEmail'));
+  }
 
-    if (!utils.isValidString(password)) {
-        throw new Error(i18next.t('LOGIN.emptyPassword'));
-    }
+  if (!utils.isValidString(password)) {
+    throw new Error(i18next.t('LOGIN.emptyPassword'));
+  }
 };
 
 const registerValidator = (email, password, firstName, lastName) => {
-
-    if (!utils.isValidString(firstName)) {
-        throw new Error(i18next.t('REGISTER.emptyFirstName'));
-    }
-
-    if (!utils.isValidString(lastName)) {
-        throw new Error(i18next.t('REGISTER.emptyLastName'));
-    }
-
-    if (!utils.isValidString(email)) {
-        throw new Error(i18next.t('LOGIN.emptyEmail'));
-    }
-
-    if (!utils.isValidString(password)) {
-        throw new Error(i18next.t('LOGIN.emptyPassword'));
-    }
-}
-
-const passwordResetValidator = (email) => {
-    if (!utils.isValidString(email)) {
-        throw new Error(i18next.t('LOGIN.emptyEmail'));
-    }
-}
-
-
-const editProfileValidator = (firstName, lastName) => {
   if (!utils.isValidString(firstName)) {
-      throw new Error(i18next.t('REGISTER.emptyFirstName'));
+    throw new Error(i18next.t('REGISTER.emptyFirstName'));
   }
 
   if (!utils.isValidString(lastName)) {
-      throw new Error(i18next.t('REGISTER.emptyLastName'));
+    throw new Error(i18next.t('REGISTER.emptyLastName'));
   }
-}
+
+  if (!utils.isValidString(email)) {
+    throw new Error(i18next.t('LOGIN.emptyEmail'));
+  }
+
+  if (!utils.isValidString(password)) {
+    throw new Error(i18next.t('LOGIN.emptyPassword'));
+  }
+};
+
+const passwordResetValidator = (email) => {
+  if (!utils.isValidString(email)) {
+    throw new Error(i18next.t('LOGIN.emptyEmail'));
+  }
+};
+
+const editProfileValidator = (firstName, lastName) => {
+  if (!utils.isValidString(firstName)) {
+    throw new Error(i18next.t('REGISTER.emptyFirstName'));
+  }
+
+  if (!utils.isValidString(lastName)) {
+    throw new Error(i18next.t('REGISTER.emptyLastName'));
+  }
+};
 
 export {
   loginValidator,
