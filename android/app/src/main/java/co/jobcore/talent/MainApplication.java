8477c34016f2ac3ecddbfd93be62d5b166b5a188
Angel Lacret <alacret@gmail.com>
2019-05-03 11:41:50 -0400

    a ver que pasa

--- android/app/src/main/java/co/jobcore/talent/MainApplication.java
+++ android/app/src/main/java/co/jobcore/talent/MainApplication.java
@@ -3,6 +3,9 @@ package co.jobcore.talent;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.imagepicker.ImagePickerPackage;
+import io.invertase.firebase.RNFirebasePackage;
 import com.imagepicker.ImagePickerPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
@@ -28,6 +31,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
+            new ImagePickerPackage(),
+            new RNFirebasePackage(),
             new ImagePickerPackage(),
             new RNFirebasePackage(),
             new RNFirebaseMessagingPackage(),
