d17513a13e0e8f1a51e97c8a4ba24afa98c37a54
Angel Lacret <alacret@gmail.com>
2019-06-02 17:35:53 -0400

    upgrdae

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 /** @format */
 
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+import { AppRegistry, Platform } from 'react-native'
+import moment from 'moment'
+import App from './App'
 
-import { i18next } from './src/i18n';
-import moment from 'moment';
-import 'moment-timezone';
-import 'moment/locale/es';
 
-moment.locale(i18next.language);
+import { i18next } from './src/i18n'
+import 'moment-timezone'
+import 'moment/locale/es'
 
-AppRegistry.registerComponent(appName, () => App);
+const appName = Platform.select({
+  ios: () => 'coJobcoreTalent',
+  android: () => 'jobcore',
+})()
+
+moment.locale(i18next.language)
+
+AppRegistry.registerComponent(appName, () => App)
