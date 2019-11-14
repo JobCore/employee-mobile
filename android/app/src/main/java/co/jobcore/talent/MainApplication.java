76e98583061e4e9609d44a6443618afcf54bac10
Angel Lacret <alacret@gmail.com>
2019-11-14 14:14:10 -0500

    google info plist

--- android/app/src/main/java/co/jobcore/talent/MainApplication.java
+++ android/app/src/main/java/co/jobcore/talent/MainApplication.java
@@ -3,6 +3,7 @@ package co.jobcore.talent;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNI18nPackage(),
             new RNFirebasePackage(),
             new NetInfoPackage(),
             new AsyncStoragePackage(),
