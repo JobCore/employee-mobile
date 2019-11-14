8045c2aeab700eff2ebeb86ab52283b8e70cda6d
Angel Lacret <alacret@gmail.com>
2019-11-14 16:49:55 -0500

    missing info list

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -122,7 +122,7 @@ export default {
     jobDetails: 'Job details',
     noJobs: 'No jobs were found',
     pending: 'Pending',
-    upcoming: 'Approved',
+    upcoming: 'Upcoming',
     completed: 'Completed',
     failed: 'Failed',
     review: 'Review',
