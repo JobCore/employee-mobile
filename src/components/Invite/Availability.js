29580955a76ad981e42ee52ebee96b09e94d8d3b
Esteban Contreras <estebancont11@gmail.com>
2019-11-19 11:09:10 -0400

    fix: availability model, specific time

--- src/components/Invite/Availability.js
+++ src/components/Invite/Availability.js
@@ -21,7 +21,7 @@ import { Loading } from '../../shared/components';
 import DateTimePicker from 'react-native-modal-datetime-picker';
 import moment from 'moment';
 import { ModalHeader } from '../../shared/components/ModalHeader';
-
+import { availabilityModel } from './availability-model';
 class AddAvailability extends Component {
   static navigationOptions = {
     header: null,
@@ -42,73 +42,7 @@ class AddAvailability extends Component {
       startTimePickerVisible: false,
       endTimePickerVisible: false,
       selectedAvailability: {},
-      availability: [
-        {
-          starting_at: moment()
-            .startOf('week')
-            .add('days', 4), //thursday
-          ending_at: moment()
-            .startOf('week')
-            .add('days', 4), //thursday
-          day_number: 4,
-          available: true,
-          allday: true,
-        },
-        {
-          starting_at: moment()
-            .startOf('week')
-            .add('days', 5), //friday
-          ending_at: moment()
-            .startOf('week')
-            .add('days', 5), //friday
-          day_number: 5,
-          available: true,
-          allday: true,
-        },
-        {
-          starting_at: moment().endOf('week'), // saturday
-          ending_at: moment().endOf('week'), // saturday
-          day_number: 6,
-          available: true,
-          allday: true,
-        },
-        {
-          starting_at: moment().startOf('week'), //sunday
-          ending_at: moment().startOf('week'), //sunday
-          day_number: 0,
-          available: true,
-          allday: true,
-        },
-        {
-          starting_at: moment().startOf('isoweek'), //monday
-          ending_at: moment().startOf('isoweek'), //monday
-          day_number: 1,
-          available: true,
-          allday: true,
-        },
-        {
-          starting_at: moment()
-            .startOf('week')
-            .add('days', 2), //tuesday
-          ending_at: moment()
-            .startOf('week')
-            .add('days', 2), //tuesday
-          day_number: 2,
-          available: true,
-          allday: true,
-        },
-        {
-          starting_at: moment()
-            .startOf('week')
-            .add('days', 3), //wedesday
-          ending_at: moment()
-            .startOf('week')
-            .add('days', 3), //wedesday
-          day_number: 3,
-          available: true,
-          allday: true,
-        },
-      ],
+      availability: availabilityModel,
     };
   }
 
@@ -315,7 +249,7 @@ class AddAvailability extends Component {
                               </View>
                             </View>
                           </View>
-                          {block.allday === false ? (
+                          {block.allday === false && block.available ? (
                             <View style={styles.viewPicker}>
                               <View
                                 style={{
