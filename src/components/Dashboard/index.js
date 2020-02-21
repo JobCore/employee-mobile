5d9871d3acbbc596bbfe6175156000bf82c9c5ac
Esteban Contreras <estebancont11@gmail.com>
2020-02-21 15:16:36 -0400

    fix: enableHighAccuracy

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -217,12 +217,12 @@ class DashboardScreen extends Component {
       });
     }
     getCompletedJobs('dashboard');
-    navigator.geolocation.watchPosition(
+    navigator.geolocation.getCurrentPosition(
       () => {
         log('position acquired!');
       },
       () => CustomToast('Error obtaining the lat/long!', 'danger'),
-      { maximumAge: 0, distanceFilter: 0, enableHighAccuracy: true },
+      { maximumAge: 0, enableHighAccuracy: true },
     );
 
     this.willFocusSubscription = this.props.navigation.addListener(
@@ -250,11 +250,11 @@ class DashboardScreen extends Component {
     this.onTokenRefreshListener();
     this.notificationOpenedListener();
     //
-    navigator.geolocation.watchPosition(
+    navigator.geolocation.getCurrentPosition(
       (data) => {
         log(`Dashboard:`, data);
       },
-      { maximumAge: 0, distanceFilter: 0, enableHighAccuracy: true },
+      { maximumAge: 0, enableHighAccuracy: true },
     );
   }
 
